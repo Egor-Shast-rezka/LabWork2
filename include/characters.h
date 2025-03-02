c3ef7e0c3f0969c4a2cd3acc342c7e61cd86d130
Egor-Shast-rezka <st129457@student.spbu.ru>
2025-03-02 17:26:05 +0300

    added 1 character and PlayerDecorator

--- include/characters.h
+++ include/characters.h
@@ -10,9 +10,67 @@
 #include <cstring>
 #include <cmath>
 
+
 #ifndef CHARACTERS_H
 #define CHARACTERS_H
 
 
+// -------------------------
+class PlayerDecorator : public Player {
+protected:
+    Player* player;
 
+public:
+    PlayerDecorator(Player* p);
+    virtual ~PlayerDecorator();
+
+    virtual void setName(std::string name) override;
+    virtual std::string getName() const override;
+    virtual void getNameOnDisplay() const override;
+
+    virtual void setChips(int num) override;
+    virtual void getChipsOnDisplay() const override;
+    virtual int getChips() const override;
+
+    virtual void PlaceBid(int num) override;
+
+    virtual void setCard(Card elem) override;
+    virtual std::vector<Card> getAllCards() const override;
+
+    virtual void getCardsOnDisplay() const override;
+    virtual int getCountCards() const override;
+
+    virtual void delAllCards() override;
+
+    virtual bool isBot() override;
+    virtual int BotActions(int countChips, int currentBet, int Bank, int Allin) override;
+};
+
+
+// -------------------------
+class AllSeeingPlayer : public PlayerDecorator, public Game {
+public:
+    
+    AllSeeingPlayer(Player* p);
+    
+    Card CheckCardEnemy(Game& game, int player, int card);
+    
+    Card CharacterActions(Game& game, int player, int card);
+};
+
+/*
+// -------------------------
+class CheaterPlayer : public PlayerDecorator, public Game {
+public:
+    
+    CheaterPlayer(Player* p);
+    
+    bool isCharacter() override;
+    
+    void ReplaceCardInHand(Game& game, Dealler& dealler, int card);
+    
+    int CharacterActions();
+};
+
+*/
 #endif
--- src/characters.cpp
+++ src/characters.cpp
@@ -8,3 +8,66 @@
 #include "bots.h"
 #include "characters.h"
 #include "gameMode.h"
+
+
+// -------------------------
+PlayerDecorator::PlayerDecorator(Player* p) : Player(p->getName()), player(p) {}
+
+PlayerDecorator::~PlayerDecorator() { delete player; }
+
+void PlayerDecorator::setName(std::string name) { player->setName(name); }
+
+std::string PlayerDecorator::getName() const { return player->getName(); }
+
+void PlayerDecorator::getNameOnDisplay() const { player->getNameOnDisplay(); }
+
+void PlayerDecorator::setChips(int num) { player->setChips(num); }
+
+void PlayerDecorator::getChipsOnDisplay() const { player->getChipsOnDisplay(); }
+
+int PlayerDecorator::getChips() const { return player->getChips(); }
+
+void PlayerDecorator::PlaceBid(int num) { player->PlaceBid(num); }
+
+void PlayerDecorator::setCard(Card elem) { player->setCard(elem); }
+
+std::vector<Card> PlayerDecorator::getAllCards() const { return player->getAllCards(); }
+
+void PlayerDecorator::getCardsOnDisplay() const{ player->getCardsOnDisplay(); }
+
+int PlayerDecorator::getCountCards() const { return player->getCountCards(); }
+
+void PlayerDecorator::delAllCards() { player->delAllCards(); }
+
+bool PlayerDecorator::isBot() { return player->isBot(); }
+
+int PlayerDecorator::BotActions(int countChips, int currentBet, int Bank, int Allin) {
+    return player->BotActions(countChips, currentBet, Bank, Allin);
+}
+    
+
+// -----------------
+AllSeeingPlayer::AllSeeingPlayer(Player* p) : PlayerDecorator(p) {}
+
+Card AllSeeingPlayer::CheckCardEnemy(Game& game, int player, int card) {
+    return players[player]->getAllCards()[card];
+}
+
+    
+Card AllSeeingPlayer::CharacterActions(Game& game, int player, int card){
+    return CheckCardEnemy(game, player, card);
+}
+
+/*
+// -----------------
+CheaterPlayer::CheaterPlayer(Player* p) : PlayerDecorator(p) {}
+
+void CheaterPlayer::ReplaceCardInHand(Game& game, Dealler& dealler, int card) {
+    std::cout << "kjefkqvkjqv\n";
+    //setCard(dealler.getAllDeck().deal());
+}
+
+int CheaterPlayer::CharacterActions(){
+    return 1;
+}
+*/
