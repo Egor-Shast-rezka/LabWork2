449c6a91d00f5e84b5dc31168ac8a2f182696f68
Egor-Shast-rezka <st129457@student.spbu.ru>
2025-03-13 12:44:00 +0300

    add new folder for Timer's main file

---
+++ TimerData/main_timer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+
+int main() {
+    std::cout << "==========Start Timer==========\n";
+
+    while (true) {
+        system("cat /tmp/timerData");
+    }
+
+    return 0;
+}
